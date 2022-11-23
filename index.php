f85143b174c32c1389d685fbf6b433379e5b323f
SirRiebs <hunterriebl@gmail.com>
2022-11-23 16:11:48 -0500

    Added to initial code

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <!doctype html>
 <html lang="en-US">
 <head>
-    <!-- Head Tags -->
-    
+    <meta charset="UTF-8">
+    <meta name="description" content="Repairs for all electronics">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Riebl Repair Team | Home</title>
+    <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+    <link rel="stylesheet" href="/css/stylesheet.css">
 </head>
 <body>
     <!-- Header -->
-
+    
     <!-- Menu -->
 
     <!-- Main Image -->
