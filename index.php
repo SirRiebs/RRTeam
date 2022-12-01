dd39b28f3637e15b30866bf5ce698f2d0daeac7a
SirRiebs <hunterriebl@gmail.com>
2022-12-01 13:34:58 -0500

    Added icons updated Home and CSS

--- index.php
+++ index.php
@@ -1,18 +1,44 @@
 <!doctype html>
 <html lang="en-US">
+
 <head>
     <meta charset="UTF-8">
     <meta name="description" content="Repairs and advice for everything tech">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Riebl Repair Team | Home</title>
-    <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+    <title>RRTeam | Home</title>
+    <link rel="icon" type="image/x-icon" href="/images/icons/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/icons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/images/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/images/icons/favicon-16x16.png">
+    <link rel="manifest" href="/images/icons/site.webmanifest">
     <link rel="stylesheet" href="/css/stylesheet.css">
 </head>
+
 <body>
-    <?php include ($_SERVER['DOCUMENT_ROOT'].'/partials/header.php');?>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/partials/header.php'); ?>
     <!-- Main Image -->
 
-    <!-- Content -->
-
+    <!-- Info Cards -->
+    <div class="infoCardContainer">
+        <!-- Repair Card -->
+        <div class="infoCard">
+            <img src="/images/repairCard.jpg" alt="Repair Workbench">
+            <h4>Technology Repairs</h4>
+            <p>Computers, tablets, phones, and more.</p>
+        </div>
+        <!-- Website Card -->
+        <div class="infoCard">
+            <img src="/images/websiteCard.jpg" alt="Custom website">
+            <h4>Custom Websites</h4>
+            <p>Have a website built to fit your needs.</p>
+        </div>
+        <!-- About Card -->
+        <div class="infoCard">
+            <img src="/images/aboutCard.jpg" alt="About">
+            <h4>About RRTeam</h4>
+            <p>Learn about what we can do for you.</p>
+        </div>
+    </div>
 </body>
+
 </html>
--- partials/header.php
+++ partials/header.php
@@ -1,15 +1,20 @@
 <!-- Header -->
 <header>
-  <img src="/images/logo.png" alt="RRTeam Logo">
+  <!-- Logo -->
+  <div class="headerLogo">
+    <img src="/images/logo.png" alt="RRTeam Logo">
     <h1>Riebl Repair Team</h1>
+  </div>
+  <!-- Nav -->
+  <div class="headerNav">
+    <nav>
+      <ul>
+        <li><a href='/index.php'>Home</a></li>
+        <li><a href='/repairs/'>Repairs</a></li>
+        <li><a href='/websites/'>Custom Websites</a></li>
+        <li><a href='/about/'>About</a></li>
+        <li><a href='/contact/'>Contact</a></li>
+      </ul>
+    </nav>
+  </div>
 </header>
-<!-- Nav -->
-<nav>
-  <ul>
-    <li><a href='/index.php'>Home</a></li>
-    <li><a href='/repairs/'>Repairs</a></li>
-    <li><a href='/websites/'>Custom Websites</a></li>
-    <li><a href='/about/'>About</a></li>
-    <li><a href='/contact/'>Contact</a></li>
-  </ul>
-</nav>
